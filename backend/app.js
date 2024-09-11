821ec72dec66338a3baeb9f8771a175fbd9c78e3
Alen.m <alenmullassery123@gmail.com>
2024-09-11 07:11:56 +0530

    change the frontend separately

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import path from 'path';
+// import path from 'path';
 
 dotenv.config();
 import indexRouter from './routes/index.js';
@@ -13,11 +13,11 @@ import adminRouter from './routes/admin.js';
 const app = express();
 connectDB();
 
-// Resolve __dirname for ES modules
-const __dirname = path.resolve();
+// // Resolve __dirname for ES modules
+// const __dirname = path.resolve();
 
-// Static middleware for serving React build files
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+// // Static middleware for serving React build files
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use(cors({ origin: "*",credentials: true, }));
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -28,10 +28,10 @@ app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/admin', adminRouter);
 
-// Catch-all handler to serve React's index.html file for any unknown routes
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-});
+// // Catch-all handler to serve React's index.html file for any unknown routes
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
